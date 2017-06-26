747d5c690f2c928b16ca8ace84ea8099ae10419c
Paul Fink <Paul.Fink@stat.uni-muenchen.de>
2017-06-26 16:35:49 +0200

    generation code for graphics on poster in 2015

---
+++ poster2015/unpacked-startup.R
@@ -0,0 +1,42 @@
+# for starting up the ISIPTA not-yet-package
+# from within the 'package' folder "ISIPTA"
+
+#install.packages(c("igraph", "reshape2", "colorspace", "geosphere", "rworldmap", "ggplot2", "plyr", "stringr"))
+library(igraph)
+library(reshape2)
+library(colorspace)
+library(geosphere)
+library(rworldmap)
+library(ggplot2)
+library(plyr)
+library(stringr)
+
+load("../Package/data/authors_locations.RData")
+load("../Package/data/conferences.RData")
+load("../Package/data/papers.RData")
+load("../Package/data/papers_authors.RData")
+load("../Package/data/papers_keywords.RData")
+ 
+setwd("../Package/demo")                   # depends on...
+source("simple-summary.R")        #
+source("authors-locations.R")     #                        (produces some warnings, please ignore them)
+source("country-contributions.R") # authors-locations.R    (produces some warnings, please ignore them)
+source("authors-per-paper.R")     #
+source("regular-contributors.R")  # simple-summary.R
+source("papers-per-author.R")     #
+source("coauthors-per-author.R")  # authors-per-paper.R
+source("coauthors-network.R")     # regular-contributors.R (this takes a while)
+source("coauthors-worldmap.R")    # coauthors-network.R
+source("coauthors-network2013.R") # coauthors-network.R    (produces / overwrites pdfs!!!)
+
+#setwd("./..")
+#source("./R/graph.R")
+
+# now all should be ready!
+
+
+
+
+
+
+#
