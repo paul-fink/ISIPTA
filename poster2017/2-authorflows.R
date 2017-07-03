60f5ad8a286c0c9e813958acb3a15fbcadf16801
Paul Fink (Home) <Paul.Fink@stat.uni-muenchen.de>
2017-07-03 22:24:53 +0200

    Further measures for authorflow; Git-Ignore contains now TeX specific files

--- poster2017/2-authorflows.R
+++ poster2017/2-authorflows.R
@@ -1,7 +1,14 @@
 ### 2. Author flows
-
 # Normalize with respect to the number of unique authors per year
-authorflowmelt <- melt(data.frame(year = authorflow$year, lapply(authorflow[,-(1:2)], function(x) {x/authorflow$unique*100}), check.names = FALSE), id = "year")
+authorflow_percent <- data.frame(year = authorflow$year, lapply(authorflow[,-(1:2)], function(x) {x/authorflow$unique*100}), check.names = FALSE)
+
+# average of new contributors
+mean(authorflow_percent$'New', na.rm = TRUE)
+# average of contributors of last conference
+mean(authorflow_percent$'1-step', na.rm = TRUE)
+
+
+authorflowmelt <- melt(authorflow_percent, id = "year")
 authorflowmelt$year <- ordered(authorflowmelt$year)
 authorflowmelt <- subset(authorflowmelt, !is.na(value))
 
